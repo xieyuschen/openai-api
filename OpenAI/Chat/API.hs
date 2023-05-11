b422981c04bd0572c32346dcc04f0209b798a448
xieyuschen <xieyuschen@gmail.com>
2023-05-11 16:47:42 +0800

    feature: add request and response data

---
+++ OpenAI/Chat/API.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE InstanceSigs          #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE PatternSynonyms #-}
+
+module OpenAI.Chat.API where
+import Conf ( defaultDomain, fromDomain )
+import Auth ( getAPIKey, getAPIKey' )
+
+import qualified Data.ByteString.Lazy.Char8 as S8
+import Data.ByteString as BS
+import Data.ByteString.UTF8 as BSU
+import Data.ByteString.Lazy.UTF8 as BLU
+import Req ( baseRequest )
+import Network.HTTP.Simple
+    ( getResponseBody
+      , getResponseHeader
+      , getResponseStatus
+      , httpJSON
+      , setRequestMethod
+      , setRequestPath
+      , setRequestSecure
+      , setRequestPort
+      , setRequestHost
+      , Request
+      , Response, setRequestHeaders,
+      defaultRequest, getResponseStatusCode, setRequestBearerAuth, setRequestBody, setRequestBodyJSON
+      )
+import Network.HTTP.Client.TLS   (tlsManagerSettings)
+import           Network.HTTP.Client        (defaultManagerSettings, newManager, withResponse, httpLbs, Response (responseBody))
+import           Network.HTTP.Types.Status   (statusCode)
+import Data.Aeson.Types
+    ( (.:),
+      FromJSON(parseJSON),
+      Value(Object),
+      KeyValue((.=)),
+      ToJSON(toJSON),
+      typeMismatch )
+import Control.Monad.Trans.Maybe
+import OpenAI.Models.Data
+import Data.Aeson (decode)
+import qualified Codec.Binary.UTF8.Generic as BS
+
+import UnliftIO.Exception
+import Colog (log, logError, logWarning,
+    WithLog, Message, LogAction (unLogAction), logStringStdout, (<&), HasLog, richMessageAction,
+    pattern D, pattern I, pattern W, LoggerT (runLoggerT), pattern E, (&>), logStringStderr)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Except (runExceptT)
+import OpenAI.Chat.Data ( ChatRequest (ChatRequest), Message (Message), exampleJson, ChatCompletion )
+
+chatCompletions :: IO (Maybe ChatCompletion)
+chatCompletions = do
+  ex <- runExceptT getAPIKey'
+  let Right key = ex
+  let request
+          = setRequestMethod "POST"
+          $ setRequestBearerAuth (BSU.fromString key)          
+          $ setRequestPath (BSU.fromString "v1/chat/completions")
+          $ setRequestBodyJSON exampleJson baseRequest 
+
+  manager <- liftIO $ newManager tlsManagerSettings
+  errorOrResp <- liftIO $ tryAny $ httpLbs request manager
+  
+  case errorOrResp of
+    Left e -> do
+      logStringStderr <& ("Error: fail to send request " ++ show e)
+      return Nothing
+    Right response -> do
+      if getResponseStatusCode response == 200
+        then do
+          let bodyStr = responseBody response
+          return $ Data.Aeson.decode bodyStr
+        else do
+          logStringStdout <& ("Error: " ++ show (getResponseStatusCode response) ++ " " ++ show (responseBody response))
+          return Nothing 
--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import OpenAI.Models.API ( listModels , retrieveModel)    
-import OpenAI.Models.Data ( lData, mId, ModelPermission, Model (mId), ListResp (ListResp) )
-import Data.Aeson (decode)
+import OpenAI.Chat.API(chatCompletions)
+import OpenAI.Chat.Data(respMessageContent, choiceMessages, chatCompletionChoices, exampleJson, ChatCompletion(chatCompletionId))
 import System.IO
 import Data.ByteString.Lazy.UTF8 (fromString)
-
+import Data.Aeson (decode)
+import Data.ByteString.Lazy.UTF8 as BLU
       
 main :: IO ()
 main = do
-  res <- retrieveModel "text-ada:001"  
-  case res of
+  -- res <- chatCompletions
+  -- case res of
+  --   Nothing -> putStrLn "Nothing"
+  --   Just x -> do
+  --     let messages = choiceMessages $ head $ chatCompletionChoices x
+  --     mapM_ (putStrLn . respMessageContent ) messages
+  
+  handle <- openFile "demo.json" ReadMode
+  contents <- hGetContents handle
+  print contents
+  let decodedData = Data.Aeson.decode $ BLU.fromString contents :: Maybe ChatCompletion
+  case decodedData of
     Nothing -> putStrLn "Nothing"
-    Just x -> print $ mId x
+    Just x -> print $ chatCompletionId x
